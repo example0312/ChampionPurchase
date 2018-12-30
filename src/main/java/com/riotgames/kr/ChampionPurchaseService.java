31b4f8586b3baaf98cd0bc69936098ac5aecb2a7
iye <iye@riotgames.com>
2018-12-30 22:16:26 +0900

    Add runtime exceptions for ChampionPurchaseService

--- src/main/java/com/riotgames/kr/ChampionPurchaseService.java
+++ src/main/java/com/riotgames/kr/ChampionPurchaseService.java
@@ -23,13 +23,29 @@ public class ChampionPurchaseService {
                 Champion champion = riotService.getChampion(championName);
                 int championPrice = champion.getPrice();
                 if (championPrice <= lolUser.getRp()) {
-                    // 여기서 실제로 구매를 위해 lolUser의 RP를 감소시키고, lolUser의 ownedChampions 맵에 챔피언을 추가해야 한다.
-                    // 그러나 지금은 상기 작업들이 잘 되었다고 가정하고, true를 리턴한다.
+                    // 구매를 위해 lolUser의 RP를 감소시키고, lolUser의 ownedChampions 맵에 챔피언을 추가해야 한다.
+                    lolUser.setRp(lolUser.getRp() - champion.getPrice());
+                    lolUser.addChampion(championName, champion);
+
+                    // 여기서 실제 DB 작업도 해야겠지만, 지금은 DB 작업은 잘 되었다고 가정하고, true를 리턴한다.
                     return true;
                 }
             }
+            throw new DuplicatedChampionPurchaseException("User's owned RP is not enough");
         }
 
-        throw new RuntimeException("Invalid user.");
+        throw new InvalidAuthenticatedUserException("Invalid user.");
+    }
+
+    private class DuplicatedChampionPurchaseException extends RuntimeException {
+        DuplicatedChampionPurchaseException(String message) {
+            super(message);
+        }
+    }
+
+    private class InvalidAuthenticatedUserException extends RuntimeException {
+        InvalidAuthenticatedUserException(String message) {
+            super(message);
+        }
     }
 }
--- src/test/java/ChampionPurchaseServiceTest.java
+++ src/test/java/ChampionPurchaseServiceTest.java
@@ -26,19 +26,10 @@ public class ChampionPurchaseServiceTest {
         // when
         boolean result;
         result = championPurchaseService.buyChampion(lolUser, "Garen");
-        assertTrue("유저인증실패 또는 챔피언을 이미 보유중이거나 RP가 부족합니다.", result);
-
-        result = championPurchaseService.buyChampion(lolUser, "Ahri");
-        assertFalse("유저인증에도 성공, 챔피언 미보유, 보유 RP 가 충분한 것으로 판단되었습니다.", result);
-
-        lolUser.setRp(100);
-        result = championPurchaseService.buyChampion(lolUser, "Galio");
-        assertFalse("유저인증에도 성공, 챔피언 미보유, 보유 RP 가 충분한 것으로 판단되었습니다.", result);
 
         // then
+        // TODO: Graren 구매가 성공함을 assert 문으로 작성해 보세요.
 
-        // TODO: RP 가 충분할 때, 갈리오 구매가 성공함을 assert 문으로 작성해 보세요.
-
-        // TODO: invalidUser 일 때, "MissFortune" 구매가 실패함을 assert 문으로 작성해 보세요.
+        // TODO: invalidUser 였다면 어떻게 해야 했을까?
     }
 }
